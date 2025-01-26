cdbe24d0ef4a14193bc847543285c98a98e642ad
Jennifer Trippett <jennifer@missingpiecetech.com>
2025-01-26 14:22:38 -0600

    Actually return prodcts

--- billing-functions/src/providers/stripe/billing-functions/get-plans.ts
+++ billing-functions/src/providers/stripe/billing-functions/get-plans.ts
@@ -1,19 +1,18 @@
 export default async function getPlans(stripeClient) {
-  const prices = await stripeClient.prices.list({
-    expand: ["data.product"],
-  });
+  const products = await stripeClient.products.list({});
+  return products;
 
-  return prices?.data
-    ?.map((price: Stripe.Price) => {
-      return {
-        product_name: price.product.name,
-        product_description: price.product.description,
-        active: price.product.active,
-        currency: price.currency,
-        price: price.unit_amount,
-        id: price.id,
-        interval: price.type === "one_time" ? "one_time" : price.recurring?.interval,
-      };
-    })
-    .filter((price) => price.active);
+  // return prices?.data
+  //   ?.map((price: Stripe.Price) => {
+  //     return {
+  //       product_name: price.product.name,
+  //       product_description: price.product.description,
+  //       active: price.product.active,
+  //       currency: price.currency,
+  //       price: price.unit_amount,
+  //       id: price.id,
+  //       interval: price.type === "one_time" ? "one_time" : price.recurring?.interval,
+  //     };
+  //   })
+  //   .filter((price) => price.active);
 }
