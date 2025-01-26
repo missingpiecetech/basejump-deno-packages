36091a27cc3c02f591440af5e4311856dd2ec8ca
Jennifer Trippett <jennifer@missingpiecetech.com>
2025-01-26 14:06:09 -0600

    Filter inactive prices

--- billing-functions/src/providers/stripe/billing-functions/get-plans.ts
+++ billing-functions/src/providers/stripe/billing-functions/get-plans.ts
@@ -1,17 +1,19 @@
 export default async function getPlans(stripeClient) {
   const prices = await stripeClient.prices.list({
     expand: ["data.product"],
-    active: true,
   });
 
-  return prices?.data?.map((price: Stripe.Price) => {
-    return {
-      product_name: price.product.name,
-      product_description: price.product.description,
-      currency: price.currency,
-      price: price.unit_amount,
-      id: price.id,
-      interval: price.type === "one_time" ? "one_time" : price.recurring?.interval,
-    };
-  });
+  return prices?.data
+    ?.map((price: Stripe.Price) => {
+      return {
+        product_name: price.product.name,
+        product_description: price.product.description,
+        active: price.product.active,
+        currency: price.currency,
+        price: price.unit_amount,
+        id: price.id,
+        interval: price.type === "one_time" ? "one_time" : price.recurring?.interval,
+      };
+    })
+    .filter((price) => price.active);
 }
